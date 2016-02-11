c49e1d1629238106683f84f3f8eab4546fcd7248
Timothy <timothyakampa@gmail.com>
2016-02-11 13:19:49 -0500

    cleaning up ui

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,37 +1,41 @@
 'use strict';
 
 import React, {
-  Component,
-  TouchableNativeFeedback,
-  StyleSheet,
-  Text,
-  View
+    Component,
+    TouchableNativeFeedback,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    width: 150, 
-    height: 100, 
-    backgroundColor: 'red'
-  },
-  buttonText: {
-  	margin: 30
-  }
+    buttonContainer: {
+        width: 100,
+        backgroundColor: '#1abc9c',
+        borderRadius: 5
+    },
+    buttonText: {
+        marginLeft: 30,
+        marginTop: 10,
+        marginBottom: 10,
+        color: "white"
+    }
 });
 
 class Button extends Component {
-	constructor(props) {
-		super(props)
-	}
+    constructor(props) {
+        super(props)
+    }
 
-	render(){
-		return (
-	       <TouchableNativeFeedback onPress={this.props.onPress} background={TouchableNativeFeedback.SelectableBackground()}> 
-		       <View style={styles.buttonContainer}> 
-		       	  <Text style={styles.buttonText} >{ this.props.children }</Text> 
-		       </View> 
-	       </TouchableNativeFeedback>
-    	);
-	}
+    render() {
+        return (
+            <TouchableNativeFeedback onPress={this.props.onPress}
+                                     background={TouchableNativeFeedback.SelectableBackground()}>
+                <View style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>{ this.props.children }</Text>
+                </View>
+            </TouchableNativeFeedback>
+        );
+    }
 }
 module.exports = Button;
--- app/components/ChatView.js
+++ app/components/ChatView.js
@@ -1,76 +1,80 @@
 'use strict';
 
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  TextInput,
-  View
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    TextInput,
+    View
 } from 'react-native';
 
 var Button = require('./Button');
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  textInput: {
-  	height: 40, 
-  	borderColor: 'gray', 
-  	borderWidth: 1,
-  	margin: 15
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    },
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 15
+    },
+	messageInput: {
+        margin:20
+    }
 });
 
 class ChatView extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			text: "hello!"
-		}
-		this.ws = new WebSocket('ws://10.0.3.2:3000');
-		this.send = () => {};
-		this.ws.onopen = () => { 
-			this.ws.send("register:client")
-			this.send = () => {
-				this.ws.send(this.state.text)
-				this.setState({text: ""})
-			}
-		};
-		this.ws.onmessage = (e) => {
-			this.setState({message: e.data});
-		};
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: "hello!"
+        };
+        this.ws = new WebSocket('ws://10.0.3.2:3000');
+        this.send = () => {
+        };
+        this.ws.onopen = () => {
+            this.ws.send("register:client");
+            this.send = () => {
+                this.ws.send(this.state.text);
+                this.setState({text: ""})
+            }
+        };
+        this.ws.onmessage = (e) => {
+            this.setState({message: e.data});
+        };
+    }
 
-	render(){
+    render() {
 
-		return (
-	      <View style={styles.container}>
-	        <Text style={styles.welcome}>
-	          Talk to me, please....
-	        </Text>
-	        <TextInput 
-	         	style={styles.textInput} 
-	         	value={this.state.text} 
-	         	onChangeText={(text) => this.setState({text: text})} />
-	        <Button onPress={this.send} >Send</Button>
-	        <Text>{this.state.message}</Text>
-	      </View>
-    	);
-	}
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Talk to me, please....
+                </Text>
+                <TextInput
+                    style={styles.textInput}
+                    value={this.state.text}
+                    onChangeText={(text) => this.setState({text: text})}/>
+                <Button onPress={this.send}>Send</Button>
+                <Text style={styles.messageInput}>{this.state.message}</Text>
+            </View>
+        );
+    }
 }
 module.exports = ChatView;
